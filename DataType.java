f212f5dbfe7f203a20325fc493b4098edab882ee
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-12-20 11:20:02 +0600

    enhance DataType class with additional primitive type examples and increment operations

--- DataType.java
+++ DataType.java
@@ -2,8 +2,19 @@
 class DataType {
 
     public static void main(String a[]) {
-        // Data type`
-        int num = 10;
+        // Data type
+        int i = 10;
+        byte by = 100;
+        short s = 20;
+        long l = 3025470l;
+        float f = 20.0f;
+        double d = 30.0;
+        char c = 'A';
+        boolean b = true;
+
+        c++; // character letter will be incremented by 1
+        i++;
+        System.out.println(i);
 
     }
 }
