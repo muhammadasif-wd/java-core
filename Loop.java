d98a68b0dfe0c85a29024a8f6fee03e4e35d5595
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-12-21 23:32:11 +0600

    add switch-case example to Operator class and implement Loop class with various loop structures

---
+++ Loop.java
@@ -0,0 +1,26 @@
+
+public class Loop {
+
+    public static void main(String[] args) {
+        // WHILE LOOP FOR COUNT 1 TO 10 LOVE YOU JAAN;
+
+        int love = 1;
+        while (love <= 5) {
+            System.out.println("I LOVE YOU WHILE LOOP JAAN - " + love);
+            love++;
+        }
+        // do while
+        do {
+            System.out.println("I LOVE YOU WHILE DO JAAN - " + love);
+            love++;
+        } while (love <= 10);
+
+        // for loop
+        for (int l = 1; l <= 5; l++) {
+            System.out.println("I LOVE YOU FOR LOOP JAAN - " + l);
+
+        }
+
+    }
+
+}
--- Operator.java
+++ Operator.java
@@ -17,5 +17,31 @@ class Operator {
             System.out.println("WOW! SORRY FOR THIS SITUATION");
         }
         System.out.println(result);
+
+        // switch catch using
+        int switchNumber = 5;
+        switch (switchNumber) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Saturday");
+        }
     }
+
 }
