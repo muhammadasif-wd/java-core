dc3b757a68f58d1428aa8f6ddf789e6c0d0455fb
Muhammad A SI F <96514685+muhammadasif-wd@users.noreply.github.com>
2024-12-23 17:46:58 +0600

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,42 @@
-
 class Calculator {
-
-    public static void main(String a[]) {
-        int firstNumber = 10;
-        int secondNumber = 20;
-        int sum = firstNumber + secondNumber;
-
-        System.out.println(sum);
+    // adding
+    public int adding(int num1, int num2) {
+        int result = (num1+num2);
+        return result;
     }
-
+    
+    // subtraction
+    public int sub (int num1, int num2){
+        int result = (num1 - num2);
+        return result;
+    }
+    
+    // multiplication
+    public int multi ( int num1, int num2){
+        int result = num1 * num2;
+        return result;
+    }
+    
+    // divition
+    public int divition (int num1, int num2 ){
+        int result = (num1/ num2);
+        return result;
+    }
 }
 
-// class subtraction {
-//     public static void main(String subtractionCalculate[]) {
-//         int firstNumber = 20;
-//         int lastNumber = 10;
-//         int totalSubtraction = firstNumber - lastNumber;
-//         System.out.println(totalSubtraction);
-//     }
-// }
+class Main {
+    public static void main (String[] args) {
+        Calculator obj = new Calculator();
+        
+        int addResult = obj.adding(3,3);
+        int subReuslt = obj.sub(4, 2);
+        int multi = obj.multi ( 10, 4);
+        int divi = obj.divition(10, 3);
+        
+        
+        System.out.println("Adding reuslt is " + addResult);
+        System.out.println("Subtraction reuslt is " + subReuslt);
+        System.out.println("Multiplication reuslt is " + multi);
+        System.out.println("Divition reuslt is " + divi);
+    }
+}
